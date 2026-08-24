1334dc8b5ca6b8a5f5341e2315b336b8c3547e39
nurhadi-bsc <nurhadi.supplychain@gmail.com>
2026-08-24 12:57:07 +0700

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import DashboardPage from './pages/DashboardPage';
 import KasPage from './pages/KasPage';
 import DatabaseRumahPage from './pages/DatabaseRumahPage';
 import InfoPentingPage from './pages/InfoPentingPage';
-import MigrationPage from './pages/MigrationPage';
 
 // Komponen Proteksi Halaman (Harus Login)
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
@@ -45,7 +44,6 @@ export default function App() {
           <Route path="/kas" element={<ProtectedRoute><KasPage /></ProtectedRoute>} />
           <Route path="/database" element={<ProtectedRoute><DatabaseRumahPage /></ProtectedRoute>} />
           <Route path="/info-penting" element={<ProtectedRoute><InfoPentingPage /></ProtectedRoute>} />
-          <Route path="/migrasi-data" element={<ProtectedRoute><MigrationPage /></ProtectedRoute>} />
           
           {/* Fallback jika URL tidak ditemukan */}
           <Route path="*" element={<Navigate to="/" replace />} />
